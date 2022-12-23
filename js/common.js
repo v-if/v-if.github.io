fe85c7c250a93c0f6b0b08f2d44c7139d1db2b23
Taylor <taylor@interstella.co.kr>
2022-12-23 18:10:33 +0900

    input 숫자입력 스크립트 작업

--- js/common.js
+++ js/common.js
@@ -1,3 +1,50 @@
-window.onload = function(){
-    //onload
-}
+$(window).ready(function (){
+    const input1 = document.querySelector('#input1');
+    input1.addEventListener('keyup', function(e) {
+        let value = e.target.value;
+        value = Number(value.replaceAll(',', ''));
+        if(isNaN(value)) {
+            input1.value = 0;
+        } else {
+            const formatValue = value.toLocaleString('ko-KR');
+            input1.value = formatValue;
+        }
+    });
+
+    const input2 = document.querySelector('#input2');
+    input2.addEventListener('keyup', function(e) {
+        let value = e.target.value;
+        value = Number(value.replaceAll(',', ''));
+        if(isNaN(value)) {
+            input2.value = 0;
+        } else {
+            const formatValue = value.toLocaleString('ko-KR');
+            input2.value = formatValue;
+        }
+    });
+    
+    const input3 = document.querySelector('#input3');
+    input3.addEventListener('keyup', function(e) {
+        let value = e.target.value;
+        value = Number(value.replaceAll(',', ''));
+        if(isNaN(value)) {
+            input3.value = 0;
+        } else {
+            const formatValue = value.toLocaleString('ko-KR');
+            input3.value = formatValue;
+        }
+    });
+    
+    const input4 = document.querySelector('#input4');
+    input4.addEventListener('keyup', function(e) {
+        let value = e.target.value;
+        value = Number(value.replaceAll(',', ''));
+        if(isNaN(value)) {
+            input4.value = 0;
+        } else {
+            const formatValue = value.toLocaleString('ko-KR');
+            input4.value = formatValue;
+        }
+    });
+
+});
