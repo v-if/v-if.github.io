b11013e90c2006c270cac389275ababb9ea568a4
Taylor <taylor@interstella.co.kr>
2022-12-23 16:15:17 +0900

    index.html 작업

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,4 +1,28 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    columns: [
+      { data: 'a',
+        render: function(data, type, row, meta) {
+          return meta.row + meta.settings._iDisplayStart + 1;
+        }
+      },
+      { data: 'b' },
+      { data: 'c' },
+      { data: 'd' },
+      { data: 'e' },
+      { data: 'f' }
+    ],
+    columnDefs: [
+      { className: "text-center", "targets": [0] },
+      { className: "text-right", "targets": [1,2,3,4,5] }
+    ],
+    language: {
+      paginate: {
+        next: ">",
+        previous: "<"
+      }
+    }
+  });
+
 });
