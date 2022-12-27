0cf396853b0abd69f9e00638fd5ff92c39dac9c4
Taekyu Park <xorbmail@hanmail.net>
2022-12-27 14:51:39 +0900

    Update datatables-demo.js

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -3,20 +3,31 @@ $(document).ready(function() {
   $('#dataTable').DataTable({
     searching: false, paging: false, info: false, ordering: false,
     columns: [
-      { data: 'a',
+      { data: 'round' },
+      { data: 'repayment',
         render: function(data, type, row, meta) {
-          return meta.row + meta.settings._iDisplayStart + 1;
+          return Math.round(data).toLocaleString('ko-KR');
         }
       },
-      { data: 'b' },
-      { data: 'c' },
-      { data: 'd' },
-      { data: 'e' },
-      { data: 'f' }
+      { data: 'payment',
+        render: function(data, type, row, meta) {
+          return Math.round(data).toLocaleString('ko-KR');
+        }
+      },
+      { data: 'interest',
+        render: function(data, type, row, meta) {
+          return Math.round(data).toLocaleString('ko-KR');
+        }
+      },
+      { data: 'balance',
+        render: function(data, type, row, meta) {
+          return Math.round(data).toLocaleString('ko-KR');
+        }
+      }
     ],
     columnDefs: [
       { className: "text-center", "targets": [0] },
-      { className: "text-right", "targets": [1,2,3,4,5] }
+      { className: "text-right", "targets": [1,2,3,4] }
     ],
     language: {
       paginate: {
