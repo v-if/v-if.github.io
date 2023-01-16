80eb51988cfe490e30287fcaf48dfb6cf79e9a1d
Taekyu Park <xorbmail@hanmail.net>
2023-01-16 12:43:00 +0900

    Update ticker.js

--- data/ticker.js
+++ data/ticker.js
@@ -1,14 +1,13 @@
 data = [
-    {
-        "ticker" : "AAPL"
-    },
-    {
-        "ticker" : "AMZN"
-    },
-    {
-        "ticker" : "TSLA"
-    },
-    {
-        "ticker" : "KO"
-    }
+    { "ticker" : "AAPL", "name" : "애플", "startDate" : "1980-12-12" },
+    { "ticker" : "AMZN", "name" : "아마존", "startDate" : "1997-05-15" },
+    { "ticker" : "COST", "name" : "코스트코", "startDate" : "1982-03-02" },
+    { "ticker" : "HD", "name" : "홈디포", "startDate" : "1981-09-22" },
+    { "ticker" : "KO", "name" : "코카콜라", "startDate" : "1972-06-01" },
+    { "ticker" : "LOW", "name" : "로우스", "startDate" : "1972-06-01" },
+    { "ticker" : "MSFT", "name" : "마스크로소프트", "startDate" : "1986-03-13" },
+    { "ticker" : "MNST", "name" : "몬스터 베버리지", "startDate" : "1985-12-06" },
+    { "ticker" : "SBUX", "name" : "스타벅스", "startDate" : "1992-06-26" },
+    { "ticker" : "TSLA", "name" : "테슬라", "startDate" : "2010-06-29" },
+    { "ticker" : "WMT", "name" : "월마트", "startDate" : "1972-08-25" }
 ]
