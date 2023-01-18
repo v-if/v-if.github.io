a398a389f87a7e952c04fd6616531986cf5a4579
Taekyu Park <xorbmail@hanmail.net>
2023-01-18 16:25:06 +0900

    Update ticker.js

--- data/ticker.js
+++ data/ticker.js
@@ -5,9 +5,11 @@ data = [
     { "ticker" : "HD", "name" : "홈디포", "startDate" : "1981-09-22" },
     { "ticker" : "KO", "name" : "코카콜라", "startDate" : "1972-06-01" },
     { "ticker" : "LOW", "name" : "로우스", "startDate" : "1972-06-01" },
-    { "ticker" : "MSFT", "name" : "마스크로소프트", "startDate" : "1986-03-13" },
     { "ticker" : "MNST", "name" : "몬스터 베버리지", "startDate" : "1985-12-06" },
+    { "ticker" : "MSFT", "name" : "마스크로소프트", "startDate" : "1986-03-13" },
+    { "ticker" : "QQQ", "name" : "QQQ ETF", "startDate" : "1999-03-10" },
     { "ticker" : "SBUX", "name" : "스타벅스", "startDate" : "1992-06-26" },
+    { "ticker" : "SPY", "name" : "SPY ETF", "startDate" : "1993-01-29" },
     { "ticker" : "TSLA", "name" : "테슬라", "startDate" : "2010-06-29" },
     { "ticker" : "WMT", "name" : "월마트", "startDate" : "1972-08-25" }
 ]
